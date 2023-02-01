14380a87d84b188b4b0937eef4e3516093ab1e8d
kanokpol123 <kanokpolza32@gmail.com>
2023-02-01 17:50:37 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MyController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-route/{input_data?}', function ($input_data="") {
+
+    $datas['name'] = $input_data;
+
+    return view('my_view', $datas);
+});
+
+Route::get('/my-controller/{input?}', [MyController::class, 'show']);
+
+Route::post('/my-controller', function(){
+    return "POST";
+});
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::post('/register', [RegisterController::class, 'create']);
+
+Route::get('/delete_user/{id}', [RegisterController::class, 'delete_user']);
+
+Route::get('/edit/{id}', [RegisterController::class, 'edit']);
+Route::patch('/update', [RegisterController::class, 'save_edit']);
