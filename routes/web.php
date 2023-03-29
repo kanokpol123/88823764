00bb940f52f7ba04f148a37da23726a25c83341c
kanokpol123 <kanokpolza32@gmail.com>
2023-03-30 04:27:49 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/', [IndexController::class,'index']);
